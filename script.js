4f05f79486fb8772b374a94da09f3b7ff7f5b09d
matteo <matteo.battilani20@gmail.com>
2022-12-12 19:25:11 +0100

    added reset function
    added the reset function, associated to its specific button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var counter = 0;
 var counterElement = document.getElementById("counter");
 var decrementButton = document.getElementById("decrement-button");
 var incrementButton = document.getElementById("increment-button");
+var resetButton = document.getElementById("reset-button");
 
 decrementButton.addEventListener("click", function() {
   counter--;
@@ -12,3 +13,10 @@ incrementButton.addEventListener("click", function() {
   counter++;
   counterElement.innerHTML = counter;
 });
+
+resetButton.addEventListener("click", resetCounter);
+
+function resetCounter() {
+  counter = 0;
+  counterElement.innerHTML = counter;
+}
